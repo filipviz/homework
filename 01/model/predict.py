4c3d2a239e0d04553960504daada38f45fe0e309
Filip <f@filip.world>
2023-11-11 21:22:03 -0500

    02 chapter

--- 01/model/predict.py
+++ 01/model/predict.py
@@ -6,9 +6,17 @@ warnings.filterwarnings('ignore', category=UserWarning, message=".*'has_mps' is 
 path_to_pkl = Path('./model.pkl')
 learn = load_learner(path_to_pkl)
 
-def predict_text(text):
+# print(learn.dls.vocab)
+
+def predict_text(text, n_preds=5):
     pred, pred_idx, probs = learn.predict(text)
-    print(f'Prediction: {pred}; Probability: {probs[pred_idx]:.04f}')
+    print(f'Prediction: {pred}; Probability: {(probs[pred_idx]*100.0):.02f}%.')
+
+    runner_ups = probs.argsort(descending=True)[:n_preds]
+    for idx in runner_ups:
+        if idx != pred_idx and probs[idx] > 0.1:
+            print(f"or {learn.dls.vocab[1][idx]} ({(probs[idx]*100.0):.02f}%)")
+    print()
 
 print("Enter 'exit' to quit the prediction loop.")
 while True:
